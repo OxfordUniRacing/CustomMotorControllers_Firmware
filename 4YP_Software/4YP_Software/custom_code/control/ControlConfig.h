58bda63e8919423684d756bc721bca726296a75c
Dimitar Ruzhev <dimitar.ruzhev@gmail.com>
2021-03-09 20:00:13 +0000

    Reset to old commit to fix issues with project file being corrupted. Adding files that were created in the meantime

---
+++ 4YP_Software/4YP_Software/custom_code/control/ControlConfig.h
@@ -0,0 +1,27 @@
+/*
+ * ControlConfig.h
+ *
+ * Created: 28/02/2021 18:16:05
+ *  Author: Ben
+ */ 
+
+
+#ifndef CONTROLCONFIG_H_
+#define CONTROLCONFIG_H_
+
+
+
+#define PP 15
+#define FLUX_PM 0.0067						//Calculated from experiments 
+#define LQ_TAKE_LD 0.000035
+#define LST_SQ_OMEGA_BASE_E 37.62
+#define I_MAX 170 //max current amps
+#define C1 47.8571				//FLUX_PM/4(LQ - LD)
+#define C1_SQR 2290.3				//C1 squared
+
+
+
+
+
+
+#endif /* CONTROLCONFIG_H_ */
